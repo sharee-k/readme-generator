f3ccacf497237a607611033ee1d118ad01b1acee
Sharee Burkeen <shareeburkeen@yahoo.com>
2020-08-01 18:52:29 -0500

    question array

--- index.js
+++ index.js
@@ -1,7 +1,95 @@
-// array of questions for user
-const questions = [
+const inquirer = require('inquirer');
 
-];
+// array of questions for user
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project? (Required)',
+            validate: titleInput => {
+                if (titleInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your project title!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Enter the project description (Required)',
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the project description!');
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Provide installation instructions.',
+            //when: ({ confirmAbout }) => confirmAbout
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Provide usage information.',
+            //when: ({ confirmAbout }) => confirmAbout
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: 'Enter the project contributors (Required)',
+            validate: contributionInput => {
+                if (contributionInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the project contributors!');
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'Provide test instructions.',
+            //when: ({ confirmAbout }) => confirmAbout
+        },
+        {
+            type: 'checkbox',
+            name: 'license',
+            message: 'Please choose a license from the list.',
+            choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+        },
+        {
+            type: 'input',
+            name: 'username',
+            message: 'Enter your GitHub Username (Required)',
+            validate: usernameInput => {
+                if (usernameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your GitHub Username!');
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter your email address (Required)',
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your email address!');
+                }
+            }
+        },
+    ]);
+};
 
 // function to write README file
 function writeToFile(fileName, data) {
